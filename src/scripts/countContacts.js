46d71a31202e3b8235e7f144bc5a5c4c2b08ed04
A_Oliinyk <aolejnik916@gmail.com>
2025-08-17 20:44:51 +0200

    create countContacts function

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,16 @@
-export const countContacts = async () => {};
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+
+export const countContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+    if (!Array.isArray(contacts)) return 0;
+    return contacts.length;
+  } catch (error) {
+    console.error('Error counting contacts:', error.message);
+    return 0;
+  }
+};
 
 console.log(await countContacts());
