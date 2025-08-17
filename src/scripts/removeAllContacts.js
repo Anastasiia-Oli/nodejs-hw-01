ea6fc1c2a11c787f7a8b048479d522dca578b3bd
A_Oliinyk <aolejnik916@gmail.com>
2025-08-17 21:05:24 +0200

    create removeAllContacts function

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,13 @@
-export const removeAllContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+
+export const removeAllContacts = async () => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify([], null, 2), 'utf-8');
+    console.log('All contacts removed successfully');
+  } catch (error) {
+    console.error('Error removing contacts:', error.message);
+  }
+};
 
 removeAllContacts();
