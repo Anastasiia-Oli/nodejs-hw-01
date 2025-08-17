bb1aac7642c57e7e707cfa59f4ab0afab89a99a8
A_Oliinyk <aolejnik916@gmail.com>
2025-08-17 18:38:23 +0200

    add generateContacts function

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,31 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+
+const generateContacts = async (number) => {
+  try {
+    // reading previous data
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = data ? JSON.parse(data) : [];
+
+    // creating new contacts
+    const newContacts = Array.from({ length: number }, () =>
+      createFakeContact(),
+    );
+
+    // add new contacts to previous
+    const updatedContacts = [...contacts, ...newContacts];
+
+    // rewrite file
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(updatedContacts, null, 2),
+      'utf-8',
+    );
+    console.log(`${number} of contacts successfully added`);
+  } catch (error) {
+    console.error('Error generating contacts:', error.message);
+  }
+};
 
 generateContacts(5);
