cc3d45f438f995090a0bc2542a89047ffd021e99
A_Oliinyk <aolejnik916@gmail.com>
2025-08-17 20:23:55 +0200

    create getAllContacts function

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,13 @@
-export const getAllContacts = async () => {};
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    console.log('Contacts:', data);
+  } catch (error) {
+    console.error('Error reading file:', error);
+  }
+};
 
 console.log(await getAllContacts());
