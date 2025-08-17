027ccd6344d4591553d0490a1a0af85d871b74fd
A_Oliinyk <aolejnik916@gmail.com>
2025-08-17 19:26:56 +0200

    add function to create 1 contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,25 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+
+export const addOneContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = data ? JSON.parse(data) : [];
+
+    const newContact = Array.from({ length: 1 }, () => createFakeContact());
+
+    const updatedContact = [...contacts, ...newContact];
+
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(updatedContact, null, 2),
+      'utf-8',
+    );
+    console.log('One contact successfully added');
+  } catch (error) {
+    console.error('Error generating contact:', error.message);
+  }
+};
 
 addOneContact();
